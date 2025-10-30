ca0f9cbfe658a72a8f41f0dde48c3bdc7f5c657f
sxtrng <sxtrng@gmail.com>
2025-10-30 05:59:13 -0400

    summing the individual digits in a number

---
+++ SumDigitsOfNumber.java
@@ -0,0 +1,48 @@
+/*
+    method explanation:
+
+        1.  int sum = 0;
+                The sum of the individual digits will equal this number.
+                It is set to zero to begin because we begin counting from zero.
+
+        2.  while (number != 0){}
+                In order to sum the individual digits, we must iterate through each digit.
+                Do this until the number is zero.
+
+        3.  int lastDigit = number % 10;
+                number % 10 will always yield the last digit of the number.
+
+        4.  sum += lastDigit;
+                Takes the last digit obtained from the previous step and adds it to sum.
+
+        5.  number /= 10;
+                Divides the number by ten.  Because we are using integer division,
+                any remainders are truncated, and thus never displayed.  This incrementally
+                reduces the magnitude of the number until it is equal to zero.
+
+        6.  return sum;
+                Giving the number back to the caller to be displayed.
+
+
+ */
+
+public class SumDigitsOfNumber {
+    public static void main(String[] args) {
+
+        int values = 1234;
+//        System.out.println(values % 10);
+        System.out.println(values /= 10);
+        System.out.println(sumDigits(values));
+
+    }
+
+    public static int sumDigits(int number) {
+        int sum = 0;
+        while (number != 0) {
+            int lastDigit = number % 10;
+            sum += lastDigit;
+            number /= 10;
+        }
+        return sum;
+    }
+}
