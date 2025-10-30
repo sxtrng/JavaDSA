86e9f92c56ceea63a89a9f85217abce85fcd0f83
sxtrng <sxtrng@gmail.com>
2025-10-30 06:34:24 -0400

    second method

---
+++ SumDigitsOfNumberTwo.java
@@ -0,0 +1,21 @@
+public class SumDigitsOfNumberTwo {
+    public static void main(String[] args) {
+
+        int values = 1234;
+//        System.out.println(values % 10);
+        System.out.println(values /= 10);
+        System.out.println(sumDigits(values));
+
+    }
+
+
+    public static int sumDigits(int number) {
+        String s = Integer.toString(number);
+        int sum = 0;
+
+        for (char ch : s.toCharArray()) {
+            sum += ch - '0';
+        }
+        return sum;
+    }
+}
