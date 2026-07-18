ba11053c7d08d1b65176bd3429585651224468f6
sxtrng <sxtrng@gmail.com>
2026-07-18 05:11:34 -0400

    switch two values without third variable

---
+++ SwapTwoIntegersTwo.java
@@ -0,0 +1,18 @@
+public class SwapTwoIntegersTwo {
+
+	public static void main(String[] args) {
+		
+		int valueOne = 10;
+		int valueTwo = 20;
+
+		System.out.printf("%d, %d%n", valueOne, valueTwo);
+
+		valueOne = valueOne + valueTwo;
+		valueTwo = valueOne - valueTwo;
+		valueOne = valueOne - valueTwo;
+
+		System.out.printf("%d, %d%n", valueOne, valueTwo);
+
+	}
+
+}
